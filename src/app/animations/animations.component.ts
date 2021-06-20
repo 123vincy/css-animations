df08168a1ddf6bcdb479d5f895865f403ce40b52
B Vinoth <in0136@fbspl.com>
2021-06-20 19:39:43 +0530

    new changes

--- src/app/animations/animations.component.ts
+++ src/app/animations/animations.component.ts
@@ -7,26 +7,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimationsComponent implements OnInit {
   animations = [
-    'rotating-button-effect',
-    'rotating-button-effect-1',
-    'rotating-button-effect-2',
-    'rotating-button-effect-3',
-    'rotating-button-effect-4',
-    'rotating-button-effect-5',
-    'rotating-button-effect-6',
-    'rotating-button-effect-9',
-    'rotating-button-effect-7',
-    'rotating-button-effect-8',
-    'rotating-button-effect-10',
-    'rotating-button-effect-11',
-    'rotating-button-effect-13',
-    'rotating-button-effect-14',
-    'rotating-button-effect-16',
-    'rotating-button-effect-17',
-    'rotating-button-effect-18',
-    'rotating-button-effect-19',
-    'rotating-button-effect-20',
-    'button-stretch',
+    {
+      class: 'rotating-button-effect',
+      title: 'Rotating Button Effect',
+    },
+    {
+      class: 'rotating-button-effect-1',
+      title: 'Swipe Button Effect',
+    },
+    {
+      class: 'rotating-button-effect-2',
+      title: 'Button Hover Effect',
+    },
+    {
+      class: 'rotating-button-effect-3',
+      title: 'Swipe Effect rom Top to Bottom',
+    },
+    {
+      class: 'rotating-button-effect-4',
+      title: 'Button Glowing Effect',
+    },
+    {
+      class: 'rotating-button-effect-5',
+      title: '2 Layers Button Swapping Effect',
+    },
+    {
+      class: 'rotating-button-effect-6',
+      title: 'Text Scaling Effect',
+    },
+    {
+      class: 'rotating-button-effect-9',
+      title: 'Swapping from Left to Right Effect',
+    },
+    {
+      class: 'rotating-button-effect-7',
+      title: 'Circular Swapping Button Effect',
+    },
+    {
+      class: 'rotating-button-effect-8',
+      title: '3 Layers Button Effect',
+    },
+    {
+      class: 'rotating-button-effect-10',
+      title: '3 Circles to Background Effect',
+    },
+    {
+      class: 'rotating-button-effect-11',
+      title: '3D Button Opening Effect',
+    },
+    {
+      class: 'rotating-button-effect-13',
+      title: 'Border Swapping Effect',
+    },
+    {
+      class: 'rotating-button-effect-14',
+      title: 'Border Growing Effect',
+    },
+    {
+      class: 'rotating-button-effect-16',
+      title: '4 Borders Growing Effect',
+    },
+    {
+      class: 'rotating-button-effect-17',
+      title: 'Borders Cut Scaling Effect',
+    },
+    {
+      class: 'rotating-button-effect-18',
+      title: 'Skewed Button Effect',
+    },
+    {
+      class: 'rotating-button-effect-19',
+      title: 'Pulsing Effect',
+    },
+    {
+      class: 'rotating-button-effect-20',
+      title: 'Borders Swipe Effect',
+    },
+    {
+      class: 'button-stretch',
+      title: 'Button Stretch Effect',
+    },
   ];
 
   constructor() {}
